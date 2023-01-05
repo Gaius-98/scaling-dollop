0e640c9b8e9e0ecd6921ed7c3233494b79771cb9
Grace <2388838628@qq.com>
2023-01-05 11:20:07 +0800

    feat(form):增加form预览组件

---
+++ src/components/common/EvForm/EvForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-form
+    v-bind="formConfig.formProp"
+    ref="formRef"
+    :model="formData"
+  >
+    <form-comp
+      :list="formConfig.list"
+      :data="formData"
+    >
+    </form-comp>
+  </el-form>
+</template>
+
+<script lang='ts' setup name="EvForm">
+import { reactive, toRefs, ref, PropType } from 'vue'
+import FormComp from './components/FormComp.vue'
+import type { FormInstance } from 'element-plus'
+
+const props = defineProps({
+  formConfig: {
+    type: Object as PropType<COMMON.obj>,
+    required: true,
+  },
+})
+const formRef = ref<FormInstance>()
+const formData = reactive<COMMON.obj>({})
+const { formConfig } = toRefs(props)
+const getFormData = () => formData
+const resetForm = () => {
+  if (!formRef.value) return
+  formRef.value.resetFields()
+}
+defineExpose({ getFormData, resetForm })
+</script>
+<style scoped lang='scss'>
+</style>
---
+++ src/components/common/EvForm/components/FormGrid.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-row v-bind="$attrs">
+    <el-col
+      v-for="item in cols"
+      :key="item.key"
+      :span="Number(item.span)"
+      class="col"
+    >
+      <form-comp
+        :list="item.list"
+        :data="data"
+      >
+      </form-comp>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang='ts' setup name="formGrid">
+import FormComp from './FormComp.vue'
+import { reactive, toRefs, ref, PropType } from 'vue'
+
+const props = defineProps({
+  cols: {
+    type: Array as PropType<COMMON.obj[]>,
+    required: true,
+  },
+  data: {
+    type: Object as PropType<COMMON.obj>,
+    required: true,
+  },
+})
+const { cols, data } = toRefs(props)
+</script>
+<style scoped lang='scss'>
+.col{
+  min-height: 80px;
+}
+</style>
