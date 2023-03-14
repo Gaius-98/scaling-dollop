3f7c7b940d095dd90da5d3691fdd3bc027d24837
Grace <2388838628@qq.com>
2023-03-14 16:05:30 +0800

    fix:copy增加提示

--- src/directives/common/copy.ts
+++ src/directives/common/copy.ts
@@ -1,18 +1,23 @@
+import { ElMessage } from 'element-plus'
+
 const copyText = (text:string) => {
   const inputDom = document.createElement('input') as HTMLInputElement
   inputDom.value = text
   document.body.appendChild(inputDom)
   inputDom.select()
-  document.execCommand('Copy')
+  try {
+    document.execCommand('Copy')
+    ElMessage.success('复制成功')
+  } catch (error) {
+    ElMessage.error(`复制失败:${error}`)
+  }
   document.body.removeChild(inputDom)
 }
-
 const copy = {
   name: 'copy',
   options: {
     mounted(el:HTMLElement, binding:any) {
       el.addEventListener('click', () => {
-        console.log(123)
         let text = binding.value 
         copyText(text)
       })
