5f06dad8f50b4363a2b94a6aec00aeb74d760a09
Grace <2388838628@qq.com>
2023-06-06 10:18:41 +0800

    feat：大屏增加基础组件

---
+++ src/views/page/pageDesign/components/pageHeader.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+
+<script lang='ts' setup name="pageHeader">
+import { reactive, toRefs,ref} from 'vue'
+
+</script>
+<style scoped lang='scss'>
+</style>
---
+++ src/views/page/pageDesign/components/pageSideBar.vue
@@ -0,0 +1,9 @@
+<template>
+</template>
+
+<script lang='ts' setup name="pageSideBar">
+import { reactive, toRefs, ref } from 'vue'
+
+</script>
+<style scoped lang='scss'>
+</style>
