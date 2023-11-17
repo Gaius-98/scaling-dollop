f58be04298c0bae8626d946cb060cc548d38e010
Gaius-98 <2388838628@qq.com>
2023-11-17 11:09:24 +0800

    fix:预览页未刷新请求

--- src/views/View/ViewDesign/components/ViewPreview.vue
+++ src/views/View/ViewDesign/components/ViewPreview.vue
@@ -27,12 +27,55 @@ import { useViewStore } from '@/store/viewDesign'
 import { storeToRefs } from 'pinia'
 import { useRoute } from 'vue-router'
 import api from '@/views/View/service/api'
+import useGetCompData from '@/hooks/useViewData'
+import { all } from 'axios'
 
 const store = useViewStore()
 const { viewData } = storeToRefs(store)
 const route = useRoute()
-const curData = ref<ViewData|COMMON.obj>({})
+const curData = ref<ViewData>(viewData.value)
 const loading = ref(true)
+const preHandleViewData = () => {
+  let allRes = curData.value.componentData.map(e => useGetCompData(e))
+  Promise.all(allRes).then(res => {
+    res.forEach(({ id, data }) => {
+      setCompData(id, data)
+    })
+  })
+}
+const setCompData = (id:string, data:any) => {
+  let idx = curData.value.componentData.findIndex(e => e.id == id)
+  if (idx != -1) {
+    let comp = curData.value.componentData[idx]
+    const { type } = comp
+    switch (type) {
+      case 'text':
+        curData.value.componentData[idx].props.value = data
+        break
+      case 'img':
+        curData.value.componentData[idx].props.src = data
+        break
+      case 'iframe':
+        curData.value.componentData[idx].props.url = data
+        break
+      case 'chartPie':
+        curData.value.componentData[idx].props.option.dataset.source = data
+        break
+      case 'chartBar':
+        curData.value.componentData[idx].props.option.dataset.source = data
+        break
+      case 'chartLine':
+        curData.value.componentData[idx].props.option.dataset.source = data
+        break
+      case 'icon':
+        curData.value.componentData[idx].props.icon = data
+        break
+      default:
+        curData.value.componentData[idx].props.value = data
+        break
+    }
+  }
+}
 if (route.query.id) {
   api.getDetail({
     id: route.query.id as string,
@@ -41,10 +84,12 @@ if (route.query.id) {
     loading.value = false
     if (code == 0) {
       curData.value = data
+      preHandleViewData()
     }
   })
 } else {
   curData.value = viewData.value
+  preHandleViewData()
 }
 
 const getStyle = (data:ViewComponent):StyleValue => {
